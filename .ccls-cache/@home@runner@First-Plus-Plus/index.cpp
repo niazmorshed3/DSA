25db45fd09865e8b9612d1986f3a3922602d4988
Niaz Morshed <121424001+niazmorshed3@users.noreply.github.com>
2024-06-26 21:02:24 +0000

    Add My Files

---
+++ .ccls-cache/@home@runner@First-Plus-Plus/index.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int index() {
+  cout << "Kire Sama Ki Obostha";
+  return 0 ;
+}
+  
---
+++ .ccls-cache/@home@runner@First-Plus-Plus/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // cout << "Ki obostha guys sob thikthak hai";
+  //         "\n";
+  // // cout << endl ;
+  // // cout << "\n";
+  // // int x = 76, y= 90,z = 199;
+  // // int y = 38 ;
+  // int num = 50;
+  // cout << num << '\n';
+  // ;
+  // string text = "RafiBhai";
+  // char word = 'd' ;
+  // // bool sotti = false;
+
+  // cout << text << '\n';
+  // // cout << sotti << "\n";
+  // cout << sizeof(num)  <<'\n' ;
+  // cout << sizeof(bool) <<'\n' ;
+  // cout << sizeof(word) <<'\n' ;
+  // cout << sizeof(text) <<'\n' ;
+  // ========================
+  // int marks ;
+  // cout << "Enter Marks :" <<endl ;
+  // cin >> marks ;
+  // cout << "Printing Mark is " <<marks <<"\n" ;
+  // ================
+  // string text;
+  // cout << "Please Enter Your Input" <<"\n";
+  // cin >> text ;
+  // cout << "Here Your String is " << text << endl ;
+
+  // ============
+
+  // int bhaiCount;
+  // cout << "How many brother Your Cruesh Have ?" <<'\n';
+  // cin >> bhaiCount ;
+  // if(bhaiCount <= 0){
+  //   cout<< "I will Approach" << '\n';
+  // }
+  // else {
+  //   cout << "Na bhai Dorkar nai " "Bhaai Ache: " <<bhaiCount <<" ta"  << '\n'
+  //   ;
+  // }
+  // =============
+  // int sides ;
+  // cout << "Enter Sides" << '\n' ;
+  // cin >> sides;
+  // if(sides == 3 ){
+  //   cout << "Tringle" ;
+  // }
+  // else {
+  //   cout << "Not a Tringle" ;
+  // }
+
+  // CHECK EVEN OR ODD===>..............
+
+  // int checkEvenorOdd ;
+  // cout << "Please Enter Your Number: " ;
+  // cin >> checkEvenorOdd ;
+  // if (checkEvenorOdd %2 == 0 ){
+  //   cout << "Even Number" <<endl ;
+  // }
+  // else{
+  //   cout << "Odd Number" <<endl ;
+  // }
+  // LETS TRY FOR LOOP ===>..............
+
+  // for (int i = 0; i <= 10; i++) {
+  //   cout << i << endl;
+  // }
+
+  // LETS TRY WHILR LOOP ===>..............
+  //   int i = 0;
+  //   while (i < 20) {
+
+  //     if (i == 14) {
+  //       break;
+  //     }
+  //     cout << i << endl;
+
+  //     i++;
+  //   }
+
+  // LETS TRY NASTED FOR LOOP=>>>>>>>>>>>>>>>>>>>>>>>>
+  // for (int i = 1; i<=5 ; i++ ){
+  //   cout << endl << "Outer Loop: " << i << endl <<endl ;
+  //   for (int j = 1; j <=5; j++){
+  //     cout << "Inner Loop: " <<j <<endl ;
+  //   }
+  // }
+
+  // More problems regarding for loop===================>>>>
+  // int count = 0;
+  // for (int i = 0; i<=500; i++) {
+  //   if(i % 2 == 0) {
+  //     count += 1; 
+  //     // cout << "Even Numbers: " << i <<endl ;
+  //   }
+  // }
+  // cout << "Total Even Number between 1 to 100 is " << count << endl ;
+
+  // FIRST PATTERN PROBLEM
+  
+  for (int i = 0; i < 4; i++){
+    // cout << "*" <<endl <<endl ;
+    for (int j = 0; j < 4; j++){
+      cout<< "*";
+    }
+    cout << endl;
+  }
+
+
+
+
+  
+}
